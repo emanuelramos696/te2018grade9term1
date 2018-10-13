ea5b592610285664fc0511da8ca2e7e23d93e28c
Isaac McQueen <imcqueen019@gmail.com>
2018-10-13 15:41:23 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,40 @@
 // Example
-let message = "big food!";
-console.log(message);
+let argh = "REEEEEE!";
+console.log(typeof argh);
+let oofer = "I AM TERIGGERED";
+console.log(typeof oofer);
+let woosh = "YOU TOUCHED MY BACKPACK";
+console.log(typeof woosh);
+console.log(argh);
+console.log(oofer);
+console.log(woosh);
+
+let likesAnime = true;
+console.log(typeof likesAnime);
+let likesLettuce = false;
+console.log(typeof likesLettuce);
+console.log(likesAnime);
+console.log(likesLettuce);
+
+let numAnime = 6;
+console.log(typeof numAnime);
+let numBadFoods = 15;
+console.log(typeof numBadFoods);
+let numMemes = 999;
+console.log(typeof numMemes);
+let gradePointAverage = 4;
+console.log(typeof gradePointAverage)
+console.log(numAnime);
+console.log(numBadFoods);
+console.log(numMemes);
+console.log(gradePointAverage);
+
+let oof;
+console.log(oof);
+console.log(typeof oof);
+
+let sotp = "i know it's redundant and already tells the type but i did it anyway";
+console.log(sotp);
+console.log(typeof sotp);
 
 // Type your code below!
