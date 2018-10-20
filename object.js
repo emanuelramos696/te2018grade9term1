3ecfec9ecdfd24f8e24cbe7d14430643eb74dca1
Isaac McQueen <imcqueen019@gmail.com>
2018-10-20 14:38:36 -0400

    jean Grey

---
+++ object.js
@@ -0,0 +1,24 @@
+function character(name){
+
+this.name = name;
+this.omegaLevel = false;
+this.haircolor;
+this.power;
+this.mutant = true;
+this.getInfo = getCharacter;
+}
+
+function getCharacter(){
+
+return this.name +" " + this.omegaLevel + " " + this.haircolor + " " + this.power + " " + this.mutant;
+
+}
+
+var jean = new character("Jean Grey");
+jean.omegaLevel = true;
+jean.haircolor = "orange";
+jean.power = "telekinetic";
+jean.mutant = true;
+
+
+
