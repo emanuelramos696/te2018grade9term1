28bfbd07ad387aaac2af591d5fda577c63c0c9e4
Mookie299 <daniellabillini@gmail.com>
2018-11-03 15:52:03 -0400

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -3,24 +3,23 @@ const READLINE = require("readline-sync");
 // use whatever properties you want for the player object
 let player = {
   name: undefined,
-  courage: 0,
-  intellect: 0,
-  strength: 0,
+  courage: 40,
+  intellect: 93,
+  strength: 60,
   charm: 0,
-  weapon: "fists",
+  weapon: "pipe",
   item: "flashlight",
 };
 
-console.log();
 console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
 player.name = READLINE.question("What is your name?: ");
 console.log(`${player.name} stands in front of a spooky haunted house.`);
 console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
+if(enterHouse == "yes" || enterHouse == "yes") {
   player.courage++;
-  console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-  // continue the story
+  console.log(`${player.name} prepares their ${player.weapon} and opens the door. ${player.name} finds a black figure, the face is cloaked by a black mask. the a lioght shines bright from beghind the figure revealing a 10 foot muscular beast looking down at ${player.name}`);
+ console.log(`${player.name} tries to run away. ${player.name} turns and runs for the road but get grabbed back immediatly, getting dragged into the light, then it goes dark. ${player.name} looks for another way out. there are two ways, fight the beast, or run for the door. what will you do? `)
 
 } else {
   player.intellect++;
