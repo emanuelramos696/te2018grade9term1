993ff9161f5e239cf3b7db8c68dccb5c7d328ebd
Mookie299 <daniellabillini@gmail.com>
2018-11-17 15:30:34 -0500

    Update borkieboi.js

--- borkieboi.js
+++ borkieboi.js
@@ -1,10 +1,25 @@
+
 const READLINE = require("readline-sync");
 let dog = {
-	name:"old yeller"
-	numLegs:4
-	color: "yeller"
-	breed: "golden retriever"
+	name: "old yeller",
+	numLegs: 4,
+	color: "yeller",
+	breed: "golden retriever",
 	bark: function(){
-	console.log ("bark")
-	}
-}
+	    console.log ("bark!");
+	},
+    sayName: function(){
+    	console.log (`My name is ${this.name}!`);
+    },
+    
+    eat: function (food){
+  if (food == "chicken"){
+     console.log("tasty");
+ 
+  }else{
+     console.log(`pass! ${this.name} dosen't eat ${food}s!`)
+    }
+
+} 
+
+dog.sayName();
