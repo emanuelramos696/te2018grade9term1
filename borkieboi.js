8a50f1f56edabcf0d5d2edbb91005a837ab121ed
Mookie299 <daniellabillini@gmail.com>
2018-11-17 15:12:21 -0500

    Update borkieboi.js

---
+++ borkieboi.js
@@ -0,0 +1,10 @@
+const READLINE = require("readline-sync");
+let dog = {
+	name:"old yeller"
+	numLegs:4
+	color: "yeller"
+	breed: "golden retriever"
+	bark: function(){
+	console.log ("bark")
+	}
+}
